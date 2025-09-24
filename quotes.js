da1f74ac96b1b47446ed18c10877bfba705f03cf
K-Hegli <hegli.k@gmail.com>
2025-09-24 11:48:08 +0300

    saaaa

---
+++ quotes.js
@@ -0,0 +1,70 @@
+// Motivational Quotes Collection for Omni Solutions
+// Add new quotes to this array and they'll automatically appear in the carousel
+
+const motivationalQuotes = [
+  {
+    text: "The secret of getting ahead is getting started.",
+    author: "Mark Twain"
+  },
+  {
+    text: "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+    author: "Winston Churchill"
+  },
+  {
+    text: "The only way to do great work is to love what you do.",
+    author: "Steve Jobs"
+  },
+  {
+    text: "Innovation distinguishes between a leader and a follower.",
+    author: "Steve Jobs"
+  },
+  {
+    text: "The future belongs to those who believe in the beauty of their dreams.",
+    author: "Eleanor Roosevelt"
+  },
+  {
+    text: "It is during our darkest moments that we must focus to see the light.",
+    author: "Aristotle"
+  },
+  {
+    text: "The way to get started is to quit talking and begin doing.",
+    author: "Walt Disney"
+  },
+  {
+    text: "Don't let yesterday take up too much of today.",
+    author: "Will Rogers"
+  },
+  {
+    text: "You learn more from failure than from success. Don't let it stop you. Failure builds character.",
+    author: "Unknown"
+  },
+  {
+    text: "If you are working on something that you really care about, you don't have to be pushed. The vision pulls you.",
+    author: "Steve Jobs"
+  },
+  {
+    text: "The only impossible journey is the one you never begin.",
+    author: "Tony Robbins"
+  },
+  {
+    text: "In the middle of difficulty lies opportunity.",
+    author: "Albert Einstein"
+  },
+  {
+    text: "Believe you can and you're halfway there.",
+    author: "Theodore Roosevelt"
+  },
+  {
+    text: "The only person you are destined to become is the person you decide to be.",
+    author: "Ralph Waldo Emerson"
+  },
+  {
+    text: "Opportunities don't happen. You create them.",
+    author: "Chris Grosser"
+  }
+];
+
+// Export for use in other files
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = motivationalQuotes;
+}
